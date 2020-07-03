dbd3419e6399d6f14d2309b1498b20c07e286e53
JODLYO <joe.odonnell27@gmail.com>
2020-07-03 14:10:00 +0100

    big class

--- clean_data.py
+++ clean_data.py
@@ -11,6 +11,10 @@ from albumentations import (VerticalFlip, HorizontalFlip, Flip, RandomRotate90, 
 from matplotlib import pyplot as plt
 import os
 import numpy as np
+import tensorflow as tf
+import shutil
+from tensorflow.keras.utils import Sequence
+
 #%%
 def group_boxes(row):
     return row['bbox'].str[1:-1].str.split(',', expand=True).values.astype(float)
@@ -18,8 +22,8 @@ def group_boxes(row):
 #load_image and change size
 
 
-class DataGrenerator:
-    def __init__(self, df_bboxes, img_path, batch_size, augment, new_img_shape):
+class DataGrenerator(Sequence):
+    def __init__(self, df_bboxes, img_path, batch_size, augment, new_img_shape, shuffle):
         self.df_bboxes = df_bboxes
         self.img_path = img_path
         self.batch_size = batch_size
@@ -30,6 +34,8 @@ class DataGrenerator:
         self.bbox_cols = ['x_min', 'y_min', 'bbox_width', 'bbox_height']
         self.aug_rules = self.augment_rules()
         self.image_grid = self.form_image_grid()
+        self.shuffle = shuffle
+        self.on_epoch_end()
         
     def load_img(self, image_id):
         img = cv2.imread(train_path + image_id + '.jpg') / 255
@@ -71,7 +77,7 @@ class DataGrenerator:
             img = cv2.rectangle(img, start_point, end_point, bbox_color, bbox_thickness)
         return img
     
-    def get_data(self, batch_ids):
+    def __data_generation(self, batch_ids):
         X, y = [], []
         if self.augment:
             for image_id in batch_ids:
@@ -81,6 +87,7 @@ class DataGrenerator:
                 label_grid = self.form_label_grid(aug_result['bboxes'])
                 X.append(aug_result['image'])
                 y.append(label_grid)
+                print(image_id)
         else:
             for image_id in batch_ids:     
                 img = self.load_img(image_id)
@@ -91,14 +98,19 @@ class DataGrenerator:
                 
         return np.array(X), np.array(y)
     
-    def __getitem__(self, index):
-        batch_ids = []
-        for index_no in range(index, index + self.batch_size):
-            img_id = self.image_names[index_no]
-            batch_ids.append(img_id)
-            print(img_id)
-        X, y = self.get_data(batch_ids)
+    def __getitem__(self, index):        
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]        
+        list_IDs_temp = [self.image_names[ii] for ii in indexes]        
+        X, y = self.__data_generation(list_IDs_temp)
         return X, y
+        
+    def on_epoch_end(self):
+      self.indexes = np.arange(len(self.image_names))
+      if self.shuffle == True:
+          np.random.shuffle(self.indexes)
+          
+    def __len__(self):
+      return int(np.floor(len(self.image_names) / self.batch_size))
     
     def form_image_grid(self):    
         image_grid = np.zeros((10, 10, 4))
@@ -166,8 +178,6 @@ class DataGrenerator:
                       bbox_width_scaled, bbox_height_scaled]
         return yolo_array
         
-        
-
 def get_bbox_cols(df, bbox_cols):
     df_bbox = df['bbox'].str[1:-1].str.split(',', expand=True).astype(float)
     df_bbox = df_bbox / 4
@@ -184,13 +194,31 @@ df = pd.read_csv('train.csv')
 bbox_cols = ['x_min', 'y_min', 'bbox_width', 'bbox_height']
 
 df = get_bbox_cols(df, bbox_cols)
+
+#rounding errors mean no bbox so we sort it out - could also just remove
+df['bbox_width'] = df['bbox_width'].apply(lambda x: 1 if x==0 else x)
+df['bbox_height'] = df['bbox_height'].apply(lambda x: 1 if x==0 else x) 
+
+
+
 train_path = 'train/'
+val_path = 'val/'
+
 new_img_shape = (256, 256)
 image_id = 'b6ab77fd7'
 
+imgs = pd.Series(os.listdir(train_path))
+val_imgs = imgs.iloc[3000:].copy()
+for img_name in val_imgs:
+    os.rename(os.path.join(train_path[:-1], img_name), os.path.join(val_path, img_name))
+    shutil.move(os.path.join(train_path[:-1], img_name), os.path.join(val_path, img_name))
+    os.replace(os.path.join(train_path[:-1], img_name), os.path.join(val_path, img_name))
 
-train_DG = DataGrenerator(df, train_path, 1, True, new_img_shape)
+
+train_DG = DataGrenerator(df, train_path, 1, True, new_img_shape, False)
+val_DG = DataGrenerator(df, val_path, 1, False, new_img_shape, False)
 X, y = train_DG[10]
+test = train_DG[10]
 
 def plot_img_with_img_index_boxes(X, y):
     image = np.squeeze(X).copy()
@@ -206,6 +234,120 @@ def plot_img_with_img_index_boxes(X, y):
     
     plt.imshow(image)
 
+#%% Model
 
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same', input_shape=(256, 256, 3)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(32, (3, 3), strides=(1, 1), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(1024, (3, 3), strides=(2, 2), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=(1, 1)),
+    
+    ##### output layers #####
+    
+    tf.keras.layers.Conv2D(512, (3, 3), strides=(1, 1), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((1, 1), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(256, (3, 3), strides=(1, 1), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((1, 1), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(128, (3, 3), strides=(1, 1), padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.LeakyReLU(alpha=0.1),
+    tf.keras.layers.MaxPooling2D((1, 1), strides=(1, 1)),
+    
+    tf.keras.layers.Conv2D(5, (5, 5), strides=(1, 1), activation='sigmoid')
+])
+model.compile()
+model.summary()
+
+
+def custom_loss(y_true, y_pred):
+    prob_loss = tf.keras.losses.binary_crossentropy(
+        y_true[:,:,:,0], 
+        y_pred[:,:,:,0]
+    )
+    
+    bboxes_mask = tf.where(
+        y_true[:,:,:,0] == 0, 
+        0.5, 
+        5.0
+    )
+    
+    xy_loss = tf.keras.losses.MSE(
+        y_true[:,:,:,1:3], 
+        y_pred[:,:,:,1:3]
+    )
+    
+    wh_loss = tf.keras.losses.MSE(
+        y_true[:,:,:,3:5], 
+        y_pred[:,:,:,3:5]
+    )
+    
+    xy_loss = xy_loss * bboxes_mask
+    wh_loss = wh_loss * bboxes_mask
+    
+    return prob_loss + xy_loss + wh_loss
+
+optimiser = tf.keras.optimizers.Adam(learning_rate=0.0001)
+
+model.compile(
+    optimizer=optimiser, 
+    loss=custom_loss
+)
+
+callbacks = [
+    tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', patience=2, verbose=1),
+    tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5, verbose=1, restore_best_weights=True),
+]
+
+
+test = DataGrenerator(df, train_path, 1, True, new_img_shape, False)
+for ii in range(3000):
+    X, y = test[ii]
+
+
+history = model.fit(
+    DataGrenerator(df, train_path, 1, True, new_img_shape, False),
+    #validation_data=val_DG,
+    epochs=10,
+    callbacks=callbacks
+)
+
+tf.__version__
 
 
